0e602edc03c45e6561d0174fcb1d506ba1b0f8aa
Abhishek Kumar <er.abhirishab@gmail.com>
2025-11-17 22:53:46 +0530

    JS Lectures

---
+++ JavaScript/Exp_1/app.js
@@ -0,0 +1,70 @@
+    //If else
+// let score = prompt("Enter your score: ");
+// if(score >= 75){
+//     console.log('Ist Division');
+// }       //
+// else if(score < 75 && score >= 45){
+//     console.log("IInd Division");
+// }
+// else{
+//     console.log("Fail");
+// }
+// ---------------------------------------------------------------------------------------------
+
+///For loop
+
+// let num = prompt('Enter Number: ');
+// for(let i = 1;i<=10;i++){
+//     console.log(num + " * "+ i + " = "+i*num);
+// }
+
+
+// let num = 7;
+// let fact = 1;
+// let i = 1;
+// for(let i = 1;i<=num;i++){
+//     fact = fact * i;
+//     console.log(fact);
+// }
+// console.log(fact); 
+
+
+// let num = 7;
+// let fact = 1;
+// let i = 1;
+// while(i<=num){
+//     fact = fact* i;
+//     i++;
+// }
+// console.log(fact);
+
+// let num = 15;
+// let i = 20;
+// do{
+//     console.log('Hello Duniya');
+//     i++;
+// }
+// while(i<=num);
+
+
+let day = 5;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    default:
+        console.log("Weekend");
+        break;
+}
